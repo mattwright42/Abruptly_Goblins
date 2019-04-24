d04933bc58e592cc90d15751c2f423c6c5280cbc
Matthew Wright <matthewbwright@gmail.com>
2019-04-23 22:12:33 -0700

    adds more gamers

--- abruptly_goblins.py
+++ abruptly_goblins.py
@@ -21,3 +21,26 @@ kimberly = {'name': "Kimberly Warner",
 add_gamer(kimberly, gamers)
 
 print(gamers)
+
+# Add more gamers!
+
+add_gamer({'name': 'Thomas Nelson', 'availability': [
+          "Tuesday", "Thursday", "Saturday"]}, gamers)
+add_gamer({'name': 'Joyce Sellers', 'availability': [
+          "Monday", "Wednesday", "Friday", "Saturday"]}, gamers)
+add_gamer({'name': 'Michelle Reyes', 'availability': [
+          "Wednesday", "Thursday", "Sunday"]}, gamers)
+add_gamer({'name': 'Stephen Adams', 'availability': [
+          "Thursday", "Saturday"]}, gamers)
+add_gamer({'name': 'Joanne Lynn', 'availability': [
+          "Monday", "Thursday"]}, gamers)
+add_gamer({'name': 'Latasha Bryan', 'availability': [
+          "Monday", "Sunday"]}, gamers)
+add_gamer({'name': 'Crystal Brewer', 'availability': [
+          "Thursday", "Friday", "Saturday"]}, gamers)
+add_gamer({'name': 'James Barnes Jr.', 'availability': [
+          "Tuesday", "Wednesday", "Thursday", "Sunday"]}, gamers)
+add_gamer({'name': 'Michel Trujillo', 'availability': [
+          "Monday", "Tuesday", "Wednesday"]}, gamers)
+
+print(gamers)
